dbf1da9e84dbd3ca6b97d9c90bce31923a7dbb58
Stephanie <stephaniiehallberg@gmail.com>
2024-04-08 12:51:47 +0200

    fix: search

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import { useOrganization } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string;
-    favorites?: string;
-  };
-};
-
-const DashboardPage = ({
-  searchParams,
-}: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined
 
   return ( 
     <div className="flex-1 h-[calc(100%-80px)] p-6">
@@ -24,7 +19,7 @@ const DashboardPage = ({
       ) : (
         <BoardList
           orgId={organization.id}
-          query={searchParams}
+          query={{search, favorites }}
         />
       )}
     </div>
