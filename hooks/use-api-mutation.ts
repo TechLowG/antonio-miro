2c9ffe8a1003ca91b02a3930adbc3494427cfe32
Stephanie <stephaniiehallberg@gmail.com>
2024-04-08 13:05:09 +0200

    fix: use-api-mutation typed

--- hooks/use-api-mutation.ts
+++ hooks/use-api-mutation.ts
@@ -1,24 +1,28 @@
-import { useState } from "react";
-import { useMutation } from "convex/react";
+import { useState } from 'react';
+import { useMutation } from 'convex/react';
+import { FunctionReference, FunctionReturnType, OptionalRestArgs } from 'convex/server';
 
-export const useApiMutation = (mutationFunction: any) => {
+interface ApiMutationReturnType<Mutation extends FunctionReference<"mutation">> {
+  mutate: (...payload: OptionalRestArgs<Mutation>) => Promise<FunctionReturnType<Mutation>>;
+  pending: boolean;
+}
+
+export const useApiMutation = <Mutation extends FunctionReference<'mutation'>>(
+  mutationFunction: Mutation
+): ApiMutationReturnType<Mutation> => {
   const [pending, setPending] = useState(false);
   const apiMutation = useMutation(mutationFunction);
 
-  const mutate = (payload: any) => {
+  const mutate = (...payload: OptionalRestArgs<Mutation>) => {
     setPending(true);
-    return apiMutation(payload)
+    return apiMutation(...payload)
       .finally(() => setPending(false))
-      .then((result) => {
-        return result;
-      })
-      .catch ((error) => {
-        throw error;
-      });
+      .then(result => result)
+      .catch(error => { throw error });
   };
 
   return {
     mutate,
-    pending,
+    pending
   };
 };
